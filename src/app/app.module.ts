e9c9013634aa754023ecfbcf1bba3f1e98511b2f
Uwimana Rachel <urakhel7@gmail.com>
2019-08-23 09:32:09 +0200

    create a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
