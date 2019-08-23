b9013f989ad3c639f68d53b825bee9030ce565af
Uwimana Rachel <urakhel7@gmail.com>
2019-08-23 14:32:32 +0200

    created a class

---
+++ src/app/quotation.spec.ts
@@ -0,0 +1,7 @@
+import { Quotation } from './quotation';
+
+describe('Quotation', () => {
+  it('should create an instance', () => {
+    expect(new Quotation()).toBeTruthy();
+  });
+});
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotation } from '../quotation';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+ 
+someQuotes: Quotation[] = [
+  new Quotation(1, "If your inside doesn't change now, your outside will not change then")
+]
 
+toggleDetails(index){
+  this.someQuotes[index].showDescription = !this.someQuotes[index].showDescription;
+}
+
+addNewQuote(quote){
+  
+}
   constructor() { }
 
   ngOnInit() {
